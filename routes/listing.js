5a998a3bd691e71d93ec8de274d9b112afad2778
ashu <ashutoshvishwakarma222@gmail.com>
2025-07-01 15:59:45 +0530

    updated-changes

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,7 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer  = require('multer');
 const { storage } = require("../cloudConfig.js");
-const upload = multer({ storage });    
+const upload = multer({ storage });
 
 router
     .route("/")
